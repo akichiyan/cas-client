a8753e6aa6fbc0ec389466a497764f93881737a7
unknown <youxizhuanyongyi@yeah.net>
2018-07-10 10:09:20 +0800

    cas单点登录客户端，改造可去区分内外网

---
+++ casClientSoureceCode/src/org/jasig/cas/client/authentication/AuthenticationFilter.java
@@ -0,0 +1,216 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.cas.client.authentication;
+
+import org.jasig.cas.client.util.AbstractCasFilter;
+import org.jasig.cas.client.util.CommonUtils;
+import org.jasig.cas.client.validation.Assertion;
+
+import sun.net.util.IPAddressUtil;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Filter implementation to intercept all requests and attempt to authenticate
+ * the user by redirecting them to CAS (unless the user has a ticket).
+ * <p>
+ * This filter allows you to specify the following parameters (at either the context-level or the filter-level):
+ * <ul>
+ * <li><code>casServerLoginUrl</code> - the url to log into CAS, i.e. https://cas.rutgers.edu/login</li>
+ * <li><code>renew</code> - true/false on whether to use renew or not.</li>
+ * <li><code>gateway</code> - true/false on whether to use gateway or not.</li>
+ * </ul>
+ *
+ * <p>Please see AbstractCasFilter for additional properties.</p>
+ *
+ * @author Scott Battaglia
+ * @version $Revision: 11768 $ $Date: 2007-02-07 15:44:16 -0500 (Wed, 07 Feb 2007) $
+ * @since 3.0
+ */
+public class AuthenticationFilter extends AbstractCasFilter {
+
+    /**
+     * The URL to the CAS Server login.
+     */
+    private String casServerLoginUrl;
+    
+    
+    public static boolean isInnerNet;
+
+    /**
+     * Whether to send the renew request or not.
+     */
+    private boolean renew = false;
+
+    /**
+     * Whether to send the gateway request or not.
+     */
+    private boolean gateway = false;
+    
+    private GatewayResolver gatewayStorage = new DefaultGatewayResolverImpl();
+
+    protected void initInternal(final FilterConfig filterConfig) throws ServletException {
+        if (!isIgnoreInitConfiguration()) {
+            super.initInternal(filterConfig);
+            setCasServerLoginUrl(getPropertyFromInitParams(filterConfig, "casServerLoginUrl", null));
+            log.trace("Loaded CasServerLoginUrl parameter: " + this.casServerLoginUrl);
+            
+            setRenew(parseBoolean(getPropertyFromInitParams(filterConfig, "renew", "false")));
+            log.trace("Loaded renew parameter: " + this.renew);
+            setGateway(parseBoolean(getPropertyFromInitParams(filterConfig, "gateway", "false")));
+            log.trace("Loaded gateway parameter: " + this.gateway);
+
+            final String gatewayStorageClass = getPropertyFromInitParams(filterConfig, "gatewayStorageClass", null);
+            
+            if (gatewayStorageClass != null) {
+                try {
+                    this.gatewayStorage = (GatewayResolver) Class.forName(gatewayStorageClass).newInstance();
+                } catch (final Exception e) {
+                    log.error(e,e);
+                    throw new ServletException(e);
+                }
+            }
+        }
+    }
+
+    public void init() {
+        super.init();
+        CommonUtils.assertNotNull(this.casServerLoginUrl, "casServerLoginUrl cannot be null.");
+    }
+
+    public final void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+        final HttpSession session = request.getSession(false);
+        final Assertion assertion = session != null ? (Assertion) session.getAttribute(CONST_CAS_ASSERTION) : null;
+
+        String ipAddr = request.getRemoteAddr();
+        String ipUrl= request.getRequestURI();
+      
+        //ÅÐ¶ÏÄÚÍâÍø       
+        isInnerNet = internalIp(ipAddr)||ipAddr.contains("127.0.0.1")||ipAddr.contains("localhost");
+        /*if(ipAddr.contains("172.18")||ipAddr.contains("127.0")){
+        	isInnerNet = true;
+        }else{
+        	isInnerNet = false;
+        }*/
+        
+        
+        if (assertion != null||ipUrl.contains("/iheat/hips/rep")||ipUrl.contains("/iheat/hcs/ws")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String serviceUrl = constructServiceUrl(request, response);
+        final String ticket = CommonUtils.safeGetParameter(request,getArtifactParameterName());
+        final boolean wasGatewayed = this.gatewayStorage.hasGatewayedAlready(request, serviceUrl);
+        
+        if (CommonUtils.isNotBlank(ticket) || wasGatewayed) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String modifiedServiceUrl;
+
+        log.debug("no ticket and no assertion found");
+        if (this.gateway) {
+            log.debug("setting gateway attribute in session");
+            modifiedServiceUrl = this.gatewayStorage.storeGatewayInformation(request, serviceUrl);
+        } else {
+        	if(serviceUrl.endsWith("page.do")){
+        		modifiedServiceUrl = serviceUrl;
+        	}else{
+        		modifiedServiceUrl = "";
+        	}
+            
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("Constructed service url: " + modifiedServiceUrl);
+        }
+
+        final String urlToRedirectTo = CommonUtils.constructRedirectUrl(this.casServerLoginUrl, getServiceParameterName(), modifiedServiceUrl, this.renew, this.gateway);
+
+        if (log.isDebugEnabled()) {
+            log.debug("redirecting to \"" + urlToRedirectTo + "\"");
+        }
+
+        response.sendRedirect(urlToRedirectTo);
+    }
+
+    public final void setRenew(final boolean renew) {
+        this.renew = renew;
+    }
+
+    public final void setGateway(final boolean gateway) {
+        this.gateway = gateway;
+    }
+
+    public final void setCasServerLoginUrl(final String casServerLoginUrl) {
+        this.casServerLoginUrl = casServerLoginUrl;
+    }
+    
+    public final void setGatewayStorage(final GatewayResolver gatewayStorage) {
+    	this.gatewayStorage = gatewayStorage;
+    }
+    
+    public boolean internalIp(String ip) {
+	    byte[] addr = IPAddressUtil.textToNumericFormatV4(ip);
+	    return internalIp(addr);
+	}
+
+	public boolean internalIp(byte[] addr) {
+	    final byte b0 = addr[0];
+	    final byte b1 = addr[1];
+	    //10.x.x.x/8
+	    final byte SECTION_1 = 0x0A;
+	    //172.16.x.x/12
+	    final byte SECTION_2 = (byte) 0xAC;
+	    final byte SECTION_3 = (byte) 0x10;
+	    final byte SECTION_4 = (byte) 0x1F;
+	    //192.168.x.x/16
+	    final byte SECTION_5 = (byte) 0xC0;
+	    final byte SECTION_6 = (byte) 0xA8;
+	    switch (b0) {
+	        case SECTION_1:
+	            return true;
+	        case SECTION_2:
+	            if (b1 >= SECTION_3 && b1 <= SECTION_4) {
+	                return true;
+	            }
+	        case SECTION_5:
+	            switch (b1) {
+	                case SECTION_6:
+	                    return true;
+	            }
+	        default:
+	            return false;
+	    }
+	}
+}
